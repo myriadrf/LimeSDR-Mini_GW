513b1f58587edc89ce5f18e80ede67a81f3c951a
vytautasb <vytautas.buitvydas@gmail.com>
2016-05-23 10:57:26 +0300

    TX path testing

--- db/ip/sld6bb875ea/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sld6bb875ea/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 6bb799ddeabf5edd889b
+//   DESIGN_HASH   : 3e2fb8f1c1e94b9e523e
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h6bb799ddeabf5edd889b };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h3e2fb8f1c1e94b9e523e };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin

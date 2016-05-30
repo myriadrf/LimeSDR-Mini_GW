321cfd2f6456e3bc992af988a828d6a284262aff
zydrunas <z.tamosevicius@limemicro.com>
2016-05-30 15:45:23 +0300

    Generate programming files option added.

--- gen_prg_files.tcl
+++ gen_prg_files.tcl
@@ -1,1 +1,4 @@
 qexec "quartus_cpf -c output_files/gen_pof_file.cof"
+post_message "*******************************************************************"
+post_message "Generated programming file: uLimeSDR_lms7_trx.pof" -submsgs [list "Ouput file saved in /output_files directory"]
+post_message "*******************************************************************"

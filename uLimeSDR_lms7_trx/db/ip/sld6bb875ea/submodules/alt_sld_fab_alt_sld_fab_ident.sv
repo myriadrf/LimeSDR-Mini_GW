a94850d50f6d6eb8795b4e7e23bd4fbc5ddfd32b
vytautasb <vytautas.buitvydas@gmail.com>
2017-07-21 16:01:19 +0300

    Recompiled project

--- uLimeSDR_lms7_trx/db/ip/sld6bb875ea/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ uLimeSDR_lms7_trx/db/ip/sld6bb875ea/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : e8b7a58c6a27ba4ea2ee
+//   DESIGN_HASH   : c92824b51b6e6a0004d7
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'he8b7a58c6a27ba4ea2ee };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hc92824b51b6e6a0004d7 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin

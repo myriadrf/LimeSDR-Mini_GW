d74d9a02e8749f5b4251c0e1d65ece17ff1bd6a7
vytautasb <vytautas.buitvydas@gmail.com>
2017-07-21 15:40:17 +0300

    Regenerated ddrox1 IP

--- uLimeSDR_lms7_trx/ip/ddrox1/ddrox1_sim/aldec/rivierapro_setup.tcl
+++ uLimeSDR_lms7_trx/ip/ddrox1/ddrox1_sim/aldec/rivierapro_setup.tcl
@@ -1,5 +1,5 @@
 
-# (C) 2001-2016 Altera Corporation. All rights reserved.
+# (C) 2001-2017 Altera Corporation. All rights reserved.
 # Your use of Altera Corporation's design tools, logic functions and 
 # other software and tools, and its AMPP partner logic functions, and 
 # any output files any of the foregoing (including device programming 
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 15.1 193 win32 2016.05.18.14:40:44
+# ACDS 15.1 193 win32 2017.07.17.16:04:20
 # ----------------------------------------
 # Auto-generated simulation script rivierapro_setup.tcl
 # ----------------------------------------

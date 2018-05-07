57abde2397db7853c8c1215ca122d1a83a97b303
vytautasb <v.buitvydas@limemicro.com>
2018-05-07 14:01:47 +0300

    Updated bitstream generation script

--- LimeSDR-Mini_lms7_trx/gen_prg_files.tcl
+++ LimeSDR-Mini_lms7_trx/gen_prg_files.tcl
@@ -1,5 +1,5 @@
 qexec "quartus_cpf -c ../LimeSDR-Mini_bitstreams/gen_pof_file.cof"
 post_message "*******************************************************************"
-post_message "Generated programming file: LimeSDR-Mini_lms7_trx_HW_1.1.pof" -submsgs [list "Ouput file saved in ../LimeSDR-Mini_bitstreams/ directory"]
+post_message "Generated programming file: LimeSDR-Mini_lms7_trx_HW_1.2.pof" -submsgs [list "Output file saved in ../LimeSDR-Mini_bitstreams/ directory"]
 post_message "*******************************************************************"
-file copy -force -- output_files/LimeSDR-Mini_lms7_trx.sof ../LimeSDR-Mini_bitstreams/LimeSDR-Mini_lms7_trx_HW_1.1.sof
+file copy -force -- output_files/LimeSDR-Mini_lms7_trx.sof ../LimeSDR-Mini_bitstreams/LimeSDR-Mini_lms7_trx_HW_1.2.sof

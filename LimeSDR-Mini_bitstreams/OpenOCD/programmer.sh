700787845fa77e794edb334511492c511afab1ce
Luigi Cruz <luigifcruz@icloud.com>
2018-09-27 15:06:16 -0300

    OpenOCD Programming Support

---
+++ LimeSDR-Mini_bitstreams/OpenOCD/programmer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $1 == "FT2232H" ]]; then
+	openocd -f ./adapters/ft2232h.conf -c "jtag newtap 10m16 tap -expected-id 0x31030dd -irlen 8; init; svf ./LimeSDR-Mini_lms7_trx.svf; exit;"
+elif [[ $1 == "FT232H" ]]; then
+  	openocd -f ./adapters/ft232h.conf -c "jtag newtap 10m16 tap -expected-id 0x31030dd -irlen 8; init; svf ./LimeSDR-Mini_lms7_trx.svf; exit;"
+elif [[ $1 == "OLIMEX" ]]; then
+	openocd -f ./adapters/olimex.conf -c "jtag newtap 10m16 tap -expected-id 0x31030dd -irlen 8; init; svf ./LimeSDR-Mini_lms7_trx.svf; exit;"
+else 
+	echo "No matching adapters!"
+	echo "Usage: ./programmer.sh ADAPTER_NAME"
+	echo "Supported Adapters: FT2232H, FT232H & OLIMEX"
+fi
